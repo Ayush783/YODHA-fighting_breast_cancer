ccaa576e56b7df5cb29807d9f9c8d6d7ed8b3580
Avanish474 <60430980+Avanish474@users.noreply.github.com>
2020-10-05 23:52:09 +0530

    Add files via upload

---
+++ Yodha/Android/app/QuizActivity.java
@@ -0,0 +1,157 @@
+package com.example.yodha;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+
+public class QuizActivity extends AppCompatActivity {
+    public ArrayList<String> questions;
+    public TextView question;
+    public static int i = 0;
+    public static String[] arr;
+    public Button next;
+    public Button previous;
+    public RadioButton radioButton1;
+    public RadioButton radioButton2;
+    public RadioGroup radioGroup;
+    public RadioButton selectedRadioButton;
+    private Toast mToastToShow;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.quiz);
+
+        question = findViewById(R.id.textView2);
+        next = (Button) findViewById(R.id.Next);
+        previous = findViewById(R.id.Previous);
+        radioButton1 = findViewById(R.id.radioButton1);
+        radioButton2 = findViewById(R.id.radioButton2);
+        radioGroup = findViewById(R.id.radio);
+        questions = new ArrayList<>();
+
+        questions.add(getString(R.string.question_1));
+        questions.add(getString(R.string.question_2));
+        questions.add(getString(R.string.question_3));
+        questions.add(getString(R.string.question_4));
+        questions.add(getString(R.string.question_5));
+        questions.add(getString(R.string.question_6));
+        questions.add(getString(R.string.question_7));
+        questions.add(getString(R.string.question_8));
+
+
+        arr = new String[questions.size()];
+        question.setText(questions.get(0));
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+                int selectedId = radioGroup.getCheckedRadioButtonId();
+
+                selectedRadioButton = (RadioButton) findViewById(selectedId);
+                if (radioGroup.getCheckedRadioButtonId() == -1) {
+
+
+                    Toast.makeText(QuizActivity.this, "No answer selected", Toast.LENGTH_SHORT).show();
+
+
+                } else {
+                    previous.setVisibility(View.GONE);
+                    next.setX(420);
+                    i++;
+                    if (i < questions.size() - 1) {
+                        question.setText(questions.get(i));
+
+
+                    } else if (i >= questions.size() - 1 && i < questions.size()) {
+
+                        next.setText("Finish Quiz");
+                        next.setX(490);
+                        question.setText(questions.get(i));
+                    } else if (i >= questions.size()) {
+                        next.setVisibility(View.GONE);
+                        Intent intent = new Intent(QuizActivity.this, QuizResult.class);
+                        startActivity(intent);
+
+                    }
+                    boolean result = false;
+                    result = radioButton1.isChecked();
+
+
+                    radioGroup.clearCheck();
+
+                }
+            }
+        });
+
+    }
+
+    public void onRadioClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+        String str = "";
+
+        switch (view.getId()) {
+            case R.id.radioButton1:
+                if (checked) {
+                    str = "yes";
+                    arr[i] = str;
+                }
+                break;
+            case R.id.radioButton2:
+                if (checked) {
+                    str = "No";
+                    arr[i] = str;
+                }
+                break;
+
+        }
+        int toastDurationInMilliSeconds = 100;
+        mToastToShow = Toast.makeText(this, str, Toast.LENGTH_SHORT);
+
+        // Set the countdown to display the toast
+        CountDownTimer toastCountDown;
+        toastCountDown = new CountDownTimer(toastDurationInMilliSeconds, 1) {
+            public void onTick(long millisUntilFinished) {
+                mToastToShow.show();
+            }
+
+            public void onFinish() {
+                mToastToShow.cancel();
+            }
+        };
+
+        // Show the toast and starts the countdown
+        mToastToShow.show();
+        toastCountDown.start();
+
+    }
+
+    public void previousQuestion(View view) {
+
+        Intent intent = new Intent(QuizActivity.this, NavigateTo.class);
+        startActivity(intent);
+    }
+}
+
+
+
+
+
+
+
+
---
+++ Yodha/Android/app/bottom_navigation.java
@@ -0,0 +1,55 @@
+package com.example.yodha;
+
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.fragment.app.Fragment;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class bottom_navigation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_navigation);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new FragmentCall()).commit();
+        }
+    }
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.home:
+                            selectedFragment = new FragmentCall();
+                            break;
+                        case R.id.stories:
+                            selectedFragment = new StoriesFragment();
+                            break;
+                        case R.id.events:
+                            selectedFragment = new Events();
+                            break;
+                        case R.id.info:
+                              selectedFragment = new TherapyAwareness();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+}
